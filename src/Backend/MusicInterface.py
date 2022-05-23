9dc1dc41d1769dd06cc506738567046598470c20
raslanis <naftalide@gmail.com>
2022-05-23 16:13:26 +0300

    first desing for package

---
+++ src/Backend/MusicInterface.py
@@ -0,0 +1,7 @@
+#translate playlist link to ndarry of string based names for songs
+# input - string PlalistLink, output - string ndarry of songs
+
+
+
+#translate string ndarry based names for songs to playlist link
+# input - string ndarry of songs, output - string PlalistLink
