9c854688eb43528015dff094efe2db6eebbb666c
raslanis <naftalide@gmail.com>
2022-06-26 10:59:49 +0300

    improved interface, added Song type.

--- src/Backend/MusicAppInterface.py
+++ src/Backend/MusicAppInterface.py
@@ -1,9 +1,25 @@
 import numpy as np
 
 
-class MusicAppInterface:
+class Song:
+    song_name = ''
+    artist = ''
+
+    def __init__(self, song_name: str, artist: str):
+        self.artist = artist
+        self.song_name = song_name
+
+    def get_artist(self):
+        return self.artist
+
+    def get_song_name(self):
+        return self.song_name
+
+
+class MusicAppInterface():
     # input - string PlaylistLink, output - string ndarry of songs
-    def playlist_to_array(self, playlist_link: str) -> np.ndarray:
+    @staticmethod
+    def playlist_to_array(playlist_link: str) -> np.ndarray:
         """translate playlist link to string ndarry of song names"""
         pass
 
@@ -11,3 +27,9 @@ class MusicAppInterface:
     def array_to_playlist(self, song_list: np.ndarray) -> str:
         """translate string ndarry of song names to playlist link"""
         pass
+
+    # input - Song, output - platform specific song
+    def search_song(self, song: Song):
+        """searches for the song on the specific platform"""
+        pass
+
--- src/Backend/spotify.py
+++ src/Backend/spotify.py
@@ -10,22 +10,34 @@ client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-class Spotify:
+class Spotify(MusicAppInterface.MusicAppInterface):
     """translate playlist link to string ndarry of song names"""
+    def __init__(self):
+        super.__init__()
+
+    @staticmethod
     def playlist_to_array(playlist_link: str) -> np.ndarray:
         playlist = sp.playlist(playlist_link)
         songs_array = []
+        artist = ''
+        song_name = ''
         for song in playlist['tracks']['items']:
-            songs_array.append(song['track']['name'])
+            song_name = song['track']['name']
+            artist = song['track']['artists'][0]['name']
+            songs_array.append(MusicAppInterface.Song(song_name, artist))
         return np.array(songs_array)
 
     # input - string ndarry of songs, output - string PlaylistLink
-    def array_to_playlist(self, song_list: np.ndarray) -> str:
+    def array_to_playlist(self, song_list: np.ndarray, playlist_name: str) -> str:
         """translate string ndarry of song names to playlist link"""
+
         pass
 
 
-# playlist_link = 'https://open.spotify.com/playlist/37i9dQZF1DXdF699XuZIvg?si=02f0a763af604c95'
-#
-# songs_array = Spotify.playlist_to_array(playlist_link=playlist_link)
-# print(songs_array)
+playlist_link = 'https://open.spotify.com/playlist/37i9dQZF1DXdF699XuZIvg?si=02f0a763af604c95'
+
+songs_array = Spotify.playlist_to_array(playlist_link=playlist_link)
+for song in songs_array:
+    print(song.get_song_name() + ', ' + song.get_artist() + '\n')
+#user_id = sp.me()['id']
+#sp.user_playlist_create(user_id, 'first playlist attempt')
